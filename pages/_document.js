ce81b89fc607e9b5c9639bcfb0d9f02643c498f8
h.yingzi <h.yingzi@gmail.com>
2018-02-11 18:34:46 +0800

    add _document version

--- pages/_document.js
+++ pages/_document.js
@@ -3,22 +3,15 @@ import stylesheet from '../static/base.css'
 
 export default class extends Document {
   render () {
+    const production = process.env.NODE_ENV === 'production'
+    const version = production ? this.props.__NEXT_DATA__.buildStats['app.js'].hash.slice(0, 6) : ''
+
     return (
       <html>
         <Head>
           {process.env.NODE_ENV === 'production'
-            ? <link
-              rel='stylesheet'
-              type='text/css'
-              href={`/static/base.css?${this.props.__NEXT_DATA__
-                  .buildStats['app.js'].hash.slice(0, 6)}`}
-              />
-            : <style
-              global
-              dangerouslySetInnerHTML={{
-                __html: stylesheet
-              }}
-              />}
+           ? <link rel='stylesheet' type='text/css' href={`/static/base.css?${version}`} />
+           : <style global dangerouslySetInnerHTML={{ __html: stylesheet }} />}
         </Head>
         <body>
           <Main className='huiyizhuo' />
